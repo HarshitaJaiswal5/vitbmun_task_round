af67f23fdc5f70162af0c4c46acc48b04914685f
HARSHITA JAISWAL <140200820+HarshitaJaiswal5@users.noreply.github.com>
2024-12-04 03:18:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://vitbmun-task-round-one.vercel.app/",
   methods: ["POST", "GET"],
   credentials: true
 }));
