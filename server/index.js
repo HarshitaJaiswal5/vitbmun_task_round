0b65e69f96b21ed54d30e172ee22501b30aefdcc
HarshitaJaiswal5 <harshitajaiswal1704005@gmail.com>
2024-12-04 15:47:16 +0530

    added favicon

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ app.use(cors({
 
 app.use(bodyParser.json());
 
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.log('Error connecting to MongoDB:', err));
@@ -25,6 +26,11 @@ const contactSchema = new mongoose.Schema({
 
 const Contact = mongoose.model('Contact', contactSchema);
 
+app.get('/favicon.ico', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'favicon.ico'));
+});
+
+
 app.post('/contact', async (req, res) => {
   const { name, email, message } = req.body;
   const newContact = new Contact({ name, email, message });
@@ -36,6 +42,8 @@ app.post('/contact', async (req, res) => {
     res.status(500).json({ message: 'Error saving contact form data.', error: error.message });
   }
 });
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
